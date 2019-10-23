1628e5d4cf4d483e86577b6f8d87ba556a42908a
Robert Beckett <robert.beckett@onion.red>
2019-10-23 03:18:59 +0000

    Trailing spaces, trimmed result for comparison.

--- test/breedFetcherTest.js
+++ test/breedFetcherTest.js
@@ -9,7 +9,7 @@ describe('breedFetcher', () => {
 
       const expectedDesc = "The Siberians dog like temperament and affection makes the ideal lap cat and will live quite happily indoors. Very agile and powerful, the Siberian cat can easily leap and reach high places, including the tops of refrigerators and even doors.";
 
-      assert.equal(expectedDesc, desc);
+      assert.equal(expectedDesc, desc.trim());
 
       done();
     });
